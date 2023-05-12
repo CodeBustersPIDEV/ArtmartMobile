113c11ce1e30ae472a334f297547a6ff9ea2d16a
MarwenTouati1 <marwen.touati@esprit.tn>
2023-05-12 02:44:43 +0100

    comments

--- src/artmart/forms/editFormBlog.java
+++ src/artmart/forms/editFormBlog.java
@@ -21,23 +21,32 @@ import com.codename1.ui.Label;
 import com.codename1.ui.TextArea;
 import com.codename1.ui.URLImage;
 import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.list.DefaultListModel;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import net.suuft.libretranslate.Language;
+import net.suuft.libretranslate.Translator;
 
 public class editFormBlog extends BaseForm {
 
     BlogsWebService service = new BlogsWebService();
     BlogCategoriesWebService serviceCat = new BlogCategoriesWebService();
+    TextArea contentField = null;
 
     public editFormBlog(Blogs e) throws ParseException, IOException {
         this.init(Resources.getGlobalResources());
         System.out.println(e);
         Label titleField = new Label(e.getTitle(), "Title");
-        TextArea contentField = new TextArea(e.getContent());
+        titleField.getStyle().setFgColor(0x000000);
+        titleField.setWidth(380);
+        contentField = new TextArea(e.getContent());
+
 //       TextField imagefield = new TextField(e.getImage(), "image");
         Label catField = new Label(e.getCategory().getName());
+        ComboBox<Language> toComboBox = new ComboBox<>();
+        toComboBox.setModel(new DefaultListModel<>(Language.values()));
 //        ComboBox<BlogCategories> categorieField = new ComboBox<>();
 //        List<BlogCategories> categories = serviceCat.getAllCategorie();
 
@@ -59,6 +68,18 @@ public class editFormBlog extends BaseForm {
             imgUrl = URLImage.createToStorage(placeholder, filename, e.getImage());
             imageViewer = new ImageViewer(imgUrl);
         }
+        Button translateButton = new Button("Translate");
+        translateButton.addActionListener(ee -> {
+            contentField.setText(Translator.translate(Language.ENGLISH, toComboBox.getSelectedItem(), e.getContent()));
+        });
+        Button goToCommentsButton = new Button("Comment");
+        goToCommentsButton.addActionListener(ee -> {
+
+// Load the desired URL
+            String url = "http://localhost:8000/blogs/show/" + e.getId(); // Replace with the desired website URL
+            System.out.println(e.getId());
+            Display.getInstance().execute(url);
+        });
         this.add(titleField);
 
         this.add(contentField);
@@ -97,6 +118,7 @@ public class editFormBlog extends BaseForm {
                 System.out.println(ex);
             }
         });
+
         Button deleteButton = new Button("Delete");
         deleteButton.addActionListener(cc -> {
             getBlogsForm myForm = null;
@@ -110,10 +132,17 @@ public class editFormBlog extends BaseForm {
             }
         });
 
+        Container buttonContainer2 = new Container(new BoxLayout(BoxLayout.X_AXIS));
         Container buttonContainer = new Container(new BoxLayout(BoxLayout.X_AXIS));
+
+        buttonContainer2.add(toComboBox);
+        buttonContainer2.add(translateButton);
         buttonContainer.add(goToFormButton);
         buttonContainer.add(deleteButton);
+        buttonContainer.add(goToCommentsButton);
+
 //        buttonContainer.add(submitButton);
+        this.add(buttonContainer2);
         this.add(buttonContainer);
 
     }
